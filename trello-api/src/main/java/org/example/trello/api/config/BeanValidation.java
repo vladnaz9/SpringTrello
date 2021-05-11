ad2d4c7f7e2c3b275d7d09a9fca7d99f384ad6fc
Nazarychev_905 <vladnaz9@mail.ru>
2021-05-11 21:53:08 +0300

    delete validation

--- trello-api/src/main/java/org/example/trello/api/config/BeanValidation.java
+++
@@ -1,9 +0,0 @@
-//package org.example.trello.api.config;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
-//
-//public class BeanValidation {
-//    @Bean
-//    public LocalValidatorFactoryBean
-//}
